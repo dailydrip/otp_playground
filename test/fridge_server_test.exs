2c76824023a1cd3ac8a713f6f460832a759bc790
Josh Adams <josh.rubyist@gmail.com>
2017-02-21 06:14:24 -0600

    Use Elixir's GenServer module, sigh

--- test/fridge_server_test.exs
+++ test/fridge_server_test.exs
@@ -3,18 +3,18 @@ defmodule OtpPlayground.FridgeServerTest do
   alias OtpPlayground.FridgeServer
 
   test "putting something into the fridge" do
-    { :ok, fridge } = :gen_server.start_link FridgeServer, [], []
-    assert :ok == :gen_server.call(fridge, {:store, :bacon})
+    { :ok, fridge } = GenServer.start_link FridgeServer, [], []
+    assert :ok == GenServer.call(fridge, {:store, :bacon})
   end
 
   test "removing something from the fridge" do
-    { :ok, fridge } = :gen_server.start_link FridgeServer, [], []
-    :gen_server.call(fridge, {:store, :bacon})
-    assert {:ok, :bacon} == :gen_server.call(fridge, {:take, :bacon})
+    { :ok, fridge } = GenServer.start_link FridgeServer, [], []
+    GenServer.call(fridge, {:store, :bacon})
+    assert {:ok, :bacon} == GenServer.call(fridge, {:take, :bacon})
   end
 
   test "taking something from the fridge that isn't in there" do
-    { :ok, fridge } = :gen_server.start_link FridgeServer, [], []
-    assert :not_found == :gen_server.call(fridge, {:take, :bacon})
+    { :ok, fridge } = GenServer.start_link FridgeServer, [], []
+    assert :not_found == GenServer.call(fridge, {:take, :bacon})
   end
 end
